28483d9c3da9f8b4b49f068a85c2e667cad0e300
mrkrezza1 <sajit1020@gmail.com>
2023-09-23 12:25:45 +0530

    here we have added functionality in two ways

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,58 +1,92 @@
 import React from 'react'
-class MyCart extends React.Component{
-    constructor(){
+class MyCart extends React.Component {
+    constructor() {
         super();
-        this.state={
-            name:"I phone",
-            qty:5,
-            price:1000,
-            img:''
+        this.state = {
+            name: "I phone",
+            qty: 5,
+            price: 1000,
+            img: ''
         }
-        
-    }
-    increament(){
-        console.log("increament is working",this.state)
-      
-    }
-    decreament(){
 
-        console.log("decreament is working",this.state)
     }
-    deleteCart(){
-        console.log("delete is working",this.state)
-        
-     }
-  
-    render(){
-        let {name,qty,price}=this.state
-        return(
+    increament() {
+        // here setstate is the predefined key or method in which it set the state 
+        // first form
+        // this.setState({
+        //     qty: this.state.qty + 1
+        // })
+
+        // second form
+        // in this we have contained the this object prev already so we dont have to define
+        this.setState((prev) => {
+            return {
+                qty: prev.qty + 1
+            }
+        })
+        // console.log("increament is working", this.state)
+    }
+
+    decreament() {
+        this.setState(
+            {
+                qty: this.state.qty - 1
+            })
+        console.log("decreament is working", this.state)
+    }
+    deleteCart() {
+        // this is first form
+        // this.setState({
+        //     qty:this.state.qty=0
+        // })
+
+        // second form 
+        // call back method
+        this.setState((a) => {
+            if (a == 0) {
+                return {
+                    price: a.price = 0,
+                    qty: a.qty = "there has no item"
+                }
+            }
+            return {
+                qty: a.qty = 0
+            }
+        })
+        console.log("delete is working", this.state)
+
+    }
+
+    render() {
+        let { name, qty, price } = this.state
+        return (
             <div className='main'>
-            <div className='after-main'>
-                <div   className='product-image'>
-                    <img className='product-image-item' style={styles.images} src='https://t4.ftcdn.net/jpg/01/87/36/45/240_F_187364576_YxpCOYDgfAFSCDQhgysfvp4hYZ05qRWE.jpg' alt='' />
-                </div>
-                <div className='detail-main'>
-                    <div className='detail-first'>
-                        <div style={{color:"#777"}}>name: <span>{name}</span></div>
-                        <div style={{color:'#777'}}>qty: <span>{qty}</span></div>
-                        <div style={{color:"#777"}}>price:Rs. <span>{price}</span></div>
+                <div className='after-main'>
+                    <div className='product-image'>
+                        <img className='product-image-item' style={styles.images} src='https://t4.ftcdn.net/jpg/01/87/36/45/240_F_187364576_YxpCOYDgfAFSCDQhgysfvp4hYZ05qRWE.jpg' alt='' />
                     </div>
-                    <div className='action-icon'>
-                        <img onClick={this.increament.bind(this)}  src='https://cdn-icons-png.flaticon.com/128/1828/1828919.png' className='icon' alt=''></img>
-                        <img onClick={this.decreament.bind(this)} src='https://cdn-icons-png.flaticon.com/128/1828/1828899.png' className='icon' alt=''></img>
-                        <img onClick={this.deleteCart.bind(this)}  src='https://cdn-icons-png.flaticon.com/128/6861/6861362.png' className='icon' alt=''></img>
+                    <div className='detail-main'>
+                        <div className='detail-first'>
+                            <div style={{ color: "#777" }}>name: <span style={{color:"black"}}>{name}</span></div>
+                            <div style={{ color: '#777' }}>qty: <span style={{color:"black"}}>{qty}</span></div>
+                            <div style={{ color: "#777" }}>price:Rs. <span style={{color:"black"}}>{price * qty}</span></div>
+                        </div>
+                        <div className='action-icon'>
+                            <img onClick={this.increament.bind(this)} src='https://cdn-icons-png.flaticon.com/128/1828/1828919.png' className='icon' alt=''></img>
+                            <img onClick={this.decreament.bind(this)} src='https://cdn-icons-png.flaticon.com/128/1828/1828899.png' className='icon' alt=''></img>
+                            <img onClick={this.deleteCart.bind(this)} src='https://cdn-icons-png.flaticon.com/128/6861/6861362.png' className='icon' alt=''></img>
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         )
     }
 }
 
 let styles;
-styles={
-    images:{
-        background:'cyan',
+styles = {
+    images: {
+        background: 'cyan',
     }
 }
 export default MyCart
